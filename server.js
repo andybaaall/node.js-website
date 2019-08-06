b45390b3fa19598096b6ae0485dc2e86b12428cb
Andy Ball <932400399@bun2-206-11.local>
2019-08-07 11:50:48 +1200

    node server set up; so far it can handle any *.css requests.

---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+http.createServer(function(req, res){
+  // console.log(req);
+  console.log(`${req.method} request for ${req.url}`);
+
+  if (req.url === `/`){
+    fs.readFile('public/index.html', 'utf-8', function(err, data){
+      if (err) throw err;
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    });
+  }
+
+  else if (req.url.match(/.css/)) {
+    // console.log('got a match on a css file');
+    const cssPath = path.join(__dirname, 'public', req.url);
+
+    fs.readFile(cssPath, function (err, data){
+      if (err) throw err;
+      res.end(data);
+    })
+  }
+
+  else if (req.url === `/js/main.js`){
+    fs.readFile('public/js/main.js', function (err, data){
+      if (err) throw err;
+      res.end(data);
+    });
+  }
+
+  else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(`404 error`);
+    console.log(`looked for ${req.url} and didn't get told to do anything with it `);
+  }
+}).listen(3000);
